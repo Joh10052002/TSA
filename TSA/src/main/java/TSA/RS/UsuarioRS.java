9ae3dd7f4d8543443d371244be73a50b698d02fb
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-12-13 11:57:41 -0300

    13\12 13\12

--- TSA/src/main/java/TSA/RS/UsuarioRS.java
+++ TSA/src/main/java/TSA/RS/UsuarioRS.java
@@ -1,5 +1,9 @@
 package TSA.RS;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,61 +15,56 @@ import javax.ws.rs.core.PathSegment;
 import TSA.Services.UsuarioService;
 import tudo.sobre.astronomia.tcc.Usuario;
 
-@Path (" / usuario ")
+@Path(" / usuario ")
 public class UsuarioRS {
 
-		@Inject
-		private UsuarioService userService;
-		
-		@GET
-		@Path ("/nome/{id}")
-		@Produces("text/html")
-		public String getNome(@PathParam(id) Long id) {
-			return "<h1>"+userService.getByID(id).getNome()+"<h1>";
-		}
-		
-		@GET
-		@Path("/list")
-		@Produces("text/html")
-		public String list() {
-			String retorno = "<ul>";
-			List<Usuario> users = userService.getAll();
-			for (Usuario u: users) {
-				retorno += "<li>"+u.getNome()+"</li>";
-						
-			}
-			 
-			return retorno + "</ul>";
-		}
-		
-		@GET
-		@Path("/add/{user}")
-		public String add(@PathParam("user") PathSegment atributos) {
-			MultivaluedMap<String, String> matrixParameters = atributos.getMatrixParameters();
-			String nome = matrixParameters.getFirst("nome");
-			String emial = matrixParameters.getFirst("email");
-			String nascimento = matrixParameters.getFirst("nascimento");
-			String id = matrixParameters.getFirst("id");
-			String password = matrixParameters.getFirst("password");
-			String username = matrixParameters.getFirst("username");
-			Usuario u = new Usuario();
-			u.setEmail(email);
-			u.setId(id);
-			u.setNascimento(nascimento);
-			u.setNome(nome);
-			u.setPassword(password);
-			u.setUsername(username);
-			return "Salvo comsucesso";
-		}
-		
-		public UserService getUserService() {
-			return userService;
-		}
-		
-		public void setUserService(UserService userService) {
-			this.userService = userService;
-		}
-		
+	@Inject
+	private UsuarioService userService;
 
+	@GET
+	@Path("/nome/{id}")
+	@Produces("text/html")
+	public String getNome(@PathParam("id") Long id) {
+		return "<h1>" + userService.getByID(id).getNome() + "<h1>";
 	}
 
+	@GET
+	@Path("/list")
+	@Produces("text/html")
+	public String list() {
+		String retorno = "<ul>";
+		List<Usuario> users = userService.getAll();
+		for (Usuario u : users) {
+			retorno += "<li>" + u.getNome() + "</li>";
+
+		}
+
+		return retorno + "</ul>";
+	}
+
+	@GET
+	@Path("/add/{user}")
+	public String add(@PathParam("user") PathSegment atributos) throws ParseException {
+		MultivaluedMap<String, String> matrixParameters = atributos.getMatrixParameters();
+		String nome = matrixParameters.getFirst("nome");
+		String email = matrixParameters.getFirst("email");
+		String nascimento = matrixParameters.getFirst("nascimento");
+		String id = matrixParameters.getFirst("id");
+		String password = matrixParameters.getFirst("password");
+		String username = matrixParameters.getFirst("username");
+		Usuario u = new Usuario();
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		u.setEmail(email);
+		u.setId(Long.parseLong(id));
+		try {
+			u.setNascimento(format.parse(nascimento));
+		} catch (ParseException e) {
+			throw new ParseException("Errou", 1);
+		}
+		u.setNome(nome);
+		u.setPassword(password);
+		u.setUsername(username);
+		return "Salvo comsucesso";
+	}
+
+}
